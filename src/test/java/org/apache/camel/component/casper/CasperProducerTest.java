334a4b18372062b16ac0183879e8230807b131c2
Mikael Grouwet <synozwave@gmail.com>
2021-12-16 17:35:45 +0100

    wip

---
+++ src/test/java/org/apache/camel/component/casper/CasperProducerTest.java
@@ -0,0 +1,31 @@
+package org.apache.camel.component.casper;
+
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.Test;
+
+public class CasperProducerTest extends CamelTestSupport {
+   @Test
+   public void testMock() throws Exception
+   {
+      getMockEndpoint("mock:result").expectedBodiesReceived("Hello World");
+
+      template.sendBody("direct:start", "Hello World1");
+
+      assertMockEndpointsSatisfied();
+   }
+
+   @Override
+   protected RoutesBuilder createRouteBuilder()
+   {
+      return new RouteBuilder()
+             {
+                @Override
+                public void configure()
+                {
+                   from("direct:start").to("mock:result");
+                }
+             };
+   }
+}
